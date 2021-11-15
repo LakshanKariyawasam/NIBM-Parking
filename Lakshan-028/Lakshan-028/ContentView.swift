51bc3dd80589bdd03ec9f607fb73e1f71bb65c7a
Lakshan <krasikalakshan@gmail.com>
2021-11-15 12:40:32 +0530

    init

---
+++ Lakshan-028/Lakshan-028/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lakshan-028
+//
+//  Created by Mobios on 11/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lakshan-028/Lakshan-028/Lakshan_028App.swift
@@ -0,0 +1,17 @@
+//
+//  Lakshan_028App.swift
+//  Lakshan-028
+//
+//  Created by Mobios on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Lakshan_028App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
