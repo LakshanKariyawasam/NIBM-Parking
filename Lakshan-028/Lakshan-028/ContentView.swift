75c9d2573d3e22faad0bed5146146ca9206aaf1c
Rasika Lakshan <krasikalakshan@gmail.com>
2021-11-22 20:00:28 +0530

    Revert "Booking"

--- Lakshan-028/Lakshan-028/ContentView.swift
+++ Lakshan-028/Lakshan-028/ContentView.swift
@@ -8,80 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @StateObject var setting = AppSettings()
-    
+
     var body: some View {
-        
         TabView {
             HomeView()
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
                 }
-            
-            if(setting.isLoggedIn){
-                BookingView(setting: setting)
-                        .tabItem {
-                            Image(systemName: "book")
-                            Text("Booking")
-                    }
-                SettingsView(setting: setting).tabItem {
-                        Image(systemName: "gear")
-                        Text("Settings")
-                    }
-            }else{
-                if(setting.viewName=="Login"){
-                    LoginView(setting: setting)
-                            .tabItem {
-                                Image(systemName: "person")
-                                Text("Login")
-                        }
-                }else if(setting.viewName=="Register"){
-                    RegisterView(setting: setting)
-                            .tabItem {
-                                Image(systemName: "person")
-                                Text("Register")
-                            }
-                    
-                }else if(setting.viewName=="Password"){
-                    ForgetPasswordView(setting: setting)
-                            .tabItem {
-                                Image(systemName: "person")
-                                Text("Password Reset")
-                            }
+            BookingView()
+                .tabItem {
+                    Image(systemName: "book")
+                    Text("Booking")
                 }
-            }
-        }.onAppear {
-         
-             //self.getUserData()
-             
-         
-         }
-    }
-    
-    func getUserData(){
-        let controller = FirebaseController()
-        controller.getUser() {(success) -> Void in
-           let regId = success["regid"] as! Int64;
-            if(regId==0){
-               setting.isLoggedIn = false
-            }else{
-                                
-               setting.isLoggedIn = true
-            }
-            
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
         }
     }
 }
 
-class AppSettings: ObservableObject{
-    @Published var isLoggedIn = false;
-    @Published var viewName = "Login"
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
