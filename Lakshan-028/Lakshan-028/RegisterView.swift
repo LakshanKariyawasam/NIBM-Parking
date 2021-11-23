94b68d7a20fd9d239fcd9e67a9a5390926521e61
Rasika Lakshan <krasikalakshan@gmail.com>
2021-11-23 08:03:32 +0530

    Revert "Revert "Booking""

---
+++ Lakshan-028/Lakshan-028/RegisterView.swift
@@ -0,0 +1,80 @@
+//
+//  RegisterView.swift
+//  Lakshan-028
+//
+//  Created by Mobios on 11/18/21.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    
+    @State var email = "";
+    @State var password = "" ;
+    @State var name = "" ;
+    @State var nic = "" ;
+    @State var vehiNo = "" ;
+    @ObservedObject var setting : AppSettings
+    let controller = FirebaseController()
+    
+    
+    var body: some View {
+        
+        VStack(alignment: .center){
+          
+            VStack{
+                Text("Email").font(.title)
+                TextField("",text: $email)
+                    .frame(width: 250.0, height: 30.0).background(Color(.secondarySystemBackground))
+                
+                Text("Password").font(.title)
+                SecureField("",text: $password)
+                    .frame(width: 250.0, height: 30.0).background(Color(.secondarySystemBackground))
+            }
+            
+            Text("Name").font(.title)
+            TextField("",text: $name)
+                .frame(width: 250.0, height: 30.0).background(Color(.secondarySystemBackground))
+            
+            Text("NIC").font(.title)
+            TextField("",text: $nic)
+                .frame(width: 250.0, height: 30.0).background(Color(.secondarySystemBackground))
+            
+            Text("VehicleNo").font(.title)
+            TextField("",text: $vehiNo)
+                .frame(width: 250.0, height: 40.0).background(Color(.secondarySystemBackground))
+            VStack{
+                   
+                  Button(action:{
+                    controller.createUser(email: email, password: password, name: name, vehno: vehiNo, nic: nic) {(success) in
+                        if(success){
+                            setting.viewName = "Login"
+                        }else{
+                            
+                        }
+                        
+                    }
+                  }, label:{
+                      Text("Register").font(.title).fontWeight(.semibold).foregroundColor(.white).padding().frame(width: 200.0, height: 50.0).background(Color(hue: 0.756, saturation: 1.0, brightness: 0.992)).cornerRadius(/*@START_MENU_TOKEN@*/8.0/*@END_MENU_TOKEN@*/)
+                  })
+                  .padding(.top, 5.0)
+                  
+                  Button(action:{
+                    setting.viewName = "Login"
+                  }, label:{
+                      Text("Login").font(.title).fontWeight(.semibold).foregroundColor(.white).padding().frame(width: 200.0, height: 50.0).background(Color(hue: 0.647, saturation: 1.0, brightness: 0.992)).cornerRadius(/*@START_MENU_TOKEN@*/8.0/*@END_MENU_TOKEN@*/)
+                  })
+                  .padding(.top, 5.0)
+            }
+
+        }
+        
+        
+    }
+}
+
+//struct RegisterView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        RegisterView()
+//    }
+//}
--- Lakshan-028/Lakshan-028/SettingsView.swift
+++ Lakshan-028/Lakshan-028/SettingsView.swift
@@ -8,13 +8,92 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State var regNo = "";
+    @State var name = "" ;
+    @State var nic = "" ;
+    @State var vehiNo = "" ;
+    let controller = FirebaseController()
+    @ObservedObject var setting : AppSettings
+   
+
     var body: some View {
-        Text("Hello, Settings!")
+        VStack{
+            HStack( spacing: 50){
+                VStack(alignment: .leading){
+                    Text("Name")
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.leading)
+                        .padding(.top, 15.0)
+                    Text("NIC")
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.leading)
+                        .padding(.top, 30.0)
+                    Text("Reg No")
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.leading)
+                        .padding(.top, 30.0)
+                    Text("Vehicle No")
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.leading)
+                        .padding(.top, 30.0)
+                }
+                
+                VStack(alignment: .leading){
+                    Text(name)
+                        .fontWeight(.semibold)
+                        .padding(.top, 15.0)
+                    Text(nic)
+                        .fontWeight(.semibold)
+                        .padding(.top, 30.0)
+                    Text(regNo)
+                        .fontWeight(.semibold)
+                        .padding(.top, 30.0)
+                    Text(vehiNo)
+                        .fontWeight(.semibold)
+                        .padding(.top, 30.0)
+                }
+                
+            }.shadow(radius: 10 )
+            .padding([.leading, .bottom, .trailing], 20.0).foregroundColor(.black).background(Color(hue: 0.547, saturation: 0.34, brightness: 0.969)).cornerRadius(/*@START_MENU_TOKEN@*/8.0/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
+            Button(action:{
+                controller.logOut();
+                setting.isLoggedIn = false
+            }, label:{
+                Text("Logout").font(.title).fontWeight(.semibold).foregroundColor(.white).padding().frame(width: 200.0, height: 50.0).background(Color(hue: 0.647, saturation: 1.0, brightness: 0.992)).cornerRadius(/*@START_MENU_TOKEN@*/8.0/*@END_MENU_TOKEN@*/)
+            })
+            .padding(.vertical, 30.0)
+
+        }.padding(.top, 20.0).onAppear(){
+            self.getUserData()
+        }
+       
+        
+    }
+    
+    func getUserData(){
+        let controller = FirebaseController()
+        controller.getUser() {(success) -> Void in
+           let regId = success["regid"] as! Int64;
+            if(regId==0){
+               setting.isLoggedIn = false                
+            }else{
+                self.regNo = String(regId);
+                self.name = success["name"] as! String;
+                self.vehiNo = success["vehicleNo"] as! String
+                self.nic = success["nic"] as! String
+                
+               setting.isLoggedIn = true
+            }
+            
+        }
     }
 }
 
-struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView()
-    }
-}
+//struct SettingsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SettingsView()
+//    }
+//}
